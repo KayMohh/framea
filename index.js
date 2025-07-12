a9edefa8273baca271650b3eb64ebd1bab1e08d9
kaymohh <thrilltim@gmail.com>
2025-07-13 00:09:21 +0100

    testing db connection

---
+++ index.js
@@ -0,0 +1,6 @@
+const mongoose = require("mongoose");
+
+mongoose
+  .connect("mongodb://localhost/playground")
+  .then(() => console.log("connected to mongoDB...."))
+  .catch((err) => console.error("could not connect to MongoDB...", err));
